ff005f21a4380e89e5400de2f98ab94efb3e8f7e
Muhammad Salman3 <muhammad.salman3@intersyslimited.local>
2025-01-27 13:09:56 +0500

    two section home page developed

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+// Accordion js
+
+$(".powerful-pos-accordion-bottom").hide();
+
+$(".powerful-pos-accordion-area.active").each(function () {
+    $(this).find(".powerful-pos-accordion-bottom").show();
+    $(this).find(".accor-icon").removeClass("fa-plus").addClass("fa-minus");
+});
+
+$(".pow-pos-accordion-btn").click(function () {
+    var $currentFaqBox = $(this).closest(".powerful-pos-accordion-area");
+    var $accordionIcon = $(this).find(".accor-icon");
+
+    if (!$currentFaqBox.hasClass("active")) {
+        $(".powerful-pos-accordion-bottom").slideUp();
+        $(".powerful-pos-accordion-area").removeClass("active");
+        $(".pow-pos-accordion-btn .accor-icon").removeClass("fa-minus").addClass("fa-plus");
+
+        $currentFaqBox.addClass("active");
+        $currentFaqBox.find(".powerful-pos-accordion-bottom").slideDown();
+        $accordionIcon.removeClass("fa-plus").addClass("fa-minus");
+    } else {
+        $currentFaqBox.removeClass("active");
+        $currentFaqBox.find(".powerful-pos-accordion-bottom").slideUp();
+        $accordionIcon.removeClass("fa-minus").addClass("fa-plus");
+    }
+});
+
+// powerful pos tabing js
+const posTabs = document.querySelectorAll("[data-view]");
+
+posTabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        posTabs.forEach((tab) => {
+            tab.classList.remove("active");
+        });
+        $("[data-content]").removeClass("active");
+        $(`[data-content="${tab.dataset.view}"]`).addClass("active");
+        tab.classList.add("active");
+
+        const $firstAccordion = $(`[data-content="${tab.dataset.view}"] .powerful-pos-accordion-area`).first();
+        $(".powerful-pos-accordion-bottom").hide();
+        $(".powerful-pos-accordion-area").removeClass("active");
+        $(".pow-pos-accordion-btn .accor-icon").removeClass("fa-minus").addClass("fa-plus");
+
+        $firstAccordion.addClass("active");
+        $firstAccordion.find(".powerful-pos-accordion-bottom").show();
+        $firstAccordion.find(".accor-icon").removeClass("fa-plus").addClass("fa-minus");
+    });
+});
